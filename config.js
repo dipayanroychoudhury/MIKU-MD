922e1176737b3171bdf4554717dd1e6b91381b2b
dipayanroychoudhury <dipayanroychoudhury9@gmail.com>
2024-12-29 15:28:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "EC1l1RKZ#iIk20jIp2r7-3tmLs0h2qfqTCnPfenAgsis_CVXc-3w",
+SESSION_ID: process.env.SESSION_ID |lStQzQQB#klbeK9k4c48HGDOfJRgf3u2fiRG-R_RibPwzkG9hOWM| "EC1l1RKZ#iIk20jIp2r7-3tmLs0h2qfqTCnPfenAgsis_CVXc-3w",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/n5vvij.jpg",
-SUDO_NB: process.env.SUDO_NB || "263780934873",
+SUDO_NB: process.env.SUDO_NB || "918509459407",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-AUTO_VOICE: "true"
+MODE: process.env.MODE || "privet",
+AUTO_VOICE: "falce"
 };
+GIF_TO_STICKER: "true"
+STICKER_TO_GIF: "true'
+PREFIX: "~"
